b839074f116e5b2ace65dfb6b6f943ed769fd38b
chattie <christuaik@gmail.com>
2023-03-30 14:36:26 -0700

    due date, priority, delete functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener(("DOMContentLoaded"), () => {
+  document.querySelector('form'),addEventListener('submit', function(e){
+    e.preventDefault();
+    makeTask(e.target.querySelector('#new-task-description').value);
+    document.querySelector('form').reset();
+  })
 });
+
+function makeTask(task){
+  let p = document.createElement('p');
+  p.innerText = task;
+  p.className = document.querySelector('select').value;
+  document.getElementById('tasks').append(p);
+  handleDelete();
+  addDate(document.querySelector('form').querySelector('#dueDate').value);
+}
+
+function handleDelete(){
+    //make delete button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = ' x ';
+    document.getElementById('tasks').lastChild.append(deleteBtn);
+  
+    //delete task function
+    deleteBtn.addEventListener('click', (e) => {
+      deleteBtn.parentNode.remove();
+    })
+}
+
+function addDate(d){
+  let date = document.createElement('p');
+  date.innerText = 'due ' + d;
+  date.className = 'date';
+  document.getElementById('tasks').lastChild.append(date);
+}
